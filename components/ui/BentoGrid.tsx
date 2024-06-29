d9c31e63a8a6a6a8628e6dac2f0746a5deb40de1
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-29 20:24:09 +0300

    chore - bento grid done

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,18 +1,12 @@
 "use client";
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
-
-// Also install this npm i --save-dev @types/react-lottie
-// import Lottie from "react-lottie";
-
 import { cn } from "@/utils/cn";
-
-// import { BackgroundGradientAnimation } from "./GradientBg";
-// import GridGlobe from "./GridGlobe";
-// import animationData from "@/data/confetti.json";
 import { MagicButton } from "./MagicButton";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
+import Lottie from "react-lottie";
+import animationData from "@/data/confetti.json";
 
 export const BentoGrid = ({
   className,
@@ -59,17 +53,17 @@ export const BentoGridItem = ({
 
   const [copied, setCopied] = useState(false);
 
-  // const defaultOptions = {
-  //   loop: copied,
-  //   autoplay: copied,
-  //   animationData: animationData,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
+  const defaultOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "moldovanalexismihai06@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
@@ -184,7 +178,7 @@ export const BentoGridItem = ({
                 }`}
               >
                 {/* <img src="/confetti.gif" alt="confetti" /> */}
-                {/* <Lottie options={defaultOptions} height={200} width={400} /> */}
+                <Lottie options={defaultOptions} height={200} width={400} />
               </div>
 
               <MagicButton
