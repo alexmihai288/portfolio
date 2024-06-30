af1ca1874d9b954cfd2d1b1e36cc43894a042334
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-30 21:29:38 +0300

    chore - bug fixing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,13 +7,7 @@ export const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
+
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
@@ -32,7 +26,7 @@ export const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex mt-16 md:flex-row flex-col gap-3 md:gap-0 justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Adrian Hajdin
         </p>
